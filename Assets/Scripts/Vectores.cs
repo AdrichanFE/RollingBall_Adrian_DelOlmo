f89f98678027040657d7c336b4ddbd5e5128bf9a
AdrichanFE <adrielpelao_xty@hotmail.com>
2024-09-26 21:19:44 +0200

    Cosas

---
+++ Assets/Scripts/Vectores.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vectores : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.position = new Vector3(0, 0, 0); Para la posicion del objeto
+        //transform.eulerAngles = new Vector3(0, 0, 0); Para la rotacion del objeto
+        //transform.localScale=new Vector3(1, 1, 1); Para la escala del propio objeto
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
