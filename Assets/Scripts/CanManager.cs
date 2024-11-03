2efdaa620b80452a861d3c0c96c2986a98ffeddd
AdrichanFE <adrielpelao_xty@hotmail.com>
2024-11-03 14:01:58 +0100

    MenuOpciones

--- Assets/Scripts/CanManager.cs
+++ Assets/Scripts/CanManager.cs
@@ -2,10 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CanManager : MonoBehaviour
 {
+
+    [Header("Options")]
+    public Slider volumeMaster;
+    public Toggle mute;
+    public Toggle fullScreen;
+    public AudioMixer mixer;
+    private float lastVolume;
+
+    private void Awake()
+    {
+        volumeMaster.onValueChanged.AddListener(CambiarVolumen);
+    }
+
+    private void Start()
+    {
+        if (Screen.fullScreen)
+        {
+            fullScreen.isOn = true;
+        }
+        else
+        {
+            fullScreen.isOn=false;
+        }
+    }
+
+    public void ActivarFullScreen(bool fullScreen)
+    {
+        Screen.fullScreen = fullScreen;
+    }
+
+
+    public void SetMute()
+    {
+        
+        if (mute.isOn)
+        {
+            mixer.GetFloat("VolumeMaster", out lastVolume);
+            mixer.SetFloat("VolumeMaster", -80);
+        }    
+        else
+            mixer.SetFloat("VolumeMaster", lastVolume);
+
+
+    }
+    public void CambiarVolumen(float v)
+    {
+        mixer.SetFloat("VolumeMaster", v);
+    }
+
     public void EmpezarPartida()
     {
         SceneManager.LoadScene(1);
